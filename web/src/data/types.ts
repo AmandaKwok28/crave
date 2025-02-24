0e6030a0b57fd9f0892169dbe57629a5c6607222
LiamH <nhousen1@jhu.edu>
2025-02-24 10:24:40 -0500

    recipe card type

--- web/src/data/types.ts
+++ web/src/data/types.ts
@@ -3,3 +3,13 @@ export interface UserType {
     email: string;
     name: string;
 }
+
+export interface Recipe {
+    recipe_id: number;
+    title: string;
+    ingredient_list: string[];
+    instructions: string;
+    description: string;
+    user_id: number;
+    is_published: boolean;
+}
