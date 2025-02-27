3968d7e7e7a7c318aca8fc94e21c19ae5696c013
Stick <20114698+stickms@users.noreply.github.com>
2025-02-27 18:41:01 -0500

    Updated package json and apis

--- web/src/data/api.ts
+++ web/src/data/api.ts
@@ -1,6 +1,5 @@
 import { RecipeType, UserType } from "./types";
 import { API_URL } from "@/env";
-import { API_TEST_URL } from "@/env";
 
 // Fetch all users
 export const fetchUsers = async (): Promise<UserType[]> => {
@@ -15,7 +14,7 @@ export const fetchUsers = async (): Promise<UserType[]> => {
 
 // Fetch all recipies
 export const fetchRecipes = async (): Promise<RecipeType[]> => {
-const response = await fetch(`${API_TEST_URL}/recipes`);
+const response = await fetch(`${API_URL}/recipes`);
 if (!response.ok) {
   throw new Error(`API request failed! with status: ${response.status}`);
 }
@@ -25,7 +24,7 @@ return data;
 
 // Delete recipe by id
 export const deleteRecipe = async (recipe_id: number) : Promise<boolean> => {
-const response = await fetch(`${API_TEST_URL}/recipes/${recipe_id}`, {
+const response = await fetch(`${API_URL}/recipes/${recipe_id}`, {
     method: "DELETE",
 });
 if (!response.ok) {
@@ -41,7 +40,7 @@ description: string,
 ingredient_list: string[], 
 instructions: string
 ): Promise<RecipeType> => {
-const response = await fetch(`${API_TEST_URL}/recipes`, {
+const response = await fetch(`${API_URL}/recipes`, {
   method: "POST",
   headers: { "Content-Type": "application/json" },
   body: JSON.stringify({
