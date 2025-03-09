495f2867af285534d590bf8b9a33f9d1046ea9c8
alatsina <103145967+alatsina@users.noreply.github.com>
2025-03-09 17:30:01 -0400

    made filters optional for testing, added filters to get recipes api endpoint

--- api/src/routes/feed.routes.ts
+++ api/src/routes/feed.routes.ts
@@ -6,8 +6,21 @@ const router = Router();
 
 // Get all published recipes with search
 router.get('/', async (req, res) => {
-  const { search, skip, take, orderBy } = req.query
+  const { 
+    search, 
+    skip, 
+    take, 
+    orderBy,
+    difficulty,
+    mealType,
+    price,
+    cuisine,
+    allergens,
+    sources,
+    prepTime
+  } = req.query
 
+  // Searching
   const or: Prisma.RecipeWhereInput = search
     ? {
         OR: [
@@ -17,10 +30,49 @@ router.get('/', async (req, res) => {
       }
     : {}
 
+  // Meal Type
+  const mealTypeFilter: Prisma.RecipeWhereInput = mealType
+    ? { mealType: { hasSome: Array.isArray(mealType) ? mealType : [mealType] } }
+    : {};
+
+  // Price
+  const priceFilter: Prisma.RecipeWhereInput = price
+    ? { price: price as string }
+    : {};
+
+  // Cuisine
+  const cuisineFilter: Prisma.RecipeWhereInput = cuisine
+    ? { cuisine: cuisine as string }
+    : {};
+
+  // Allergens
+  const allergensFilter: Prisma.RecipeWhereInput = allergens
+    ? { allergens: { hasSome: Array.isArray(allergens) ? allergens : [allergens] } }
+    : {};
+
+  // Difficulty
+  const difficultyFilter: Prisma.RecipeWhereInput = difficulty
+    ? { difficulty: difficulty as string }
+    : {};
+
+  // Sources
+  const sourcesFilter: Prisma.RecipeWhereInput = sources
+    ? { sources: { hasSome: Array.isArray(sources) ? sources : [sources] } }
+    : {};
+
+  // Prep Time (Minutes)
+  const prepTimeFilter: Prisma.RecipeWhereInput = prepTime
+    ? {}
+    : {};
+
   const recipes = await prisma.recipe.findMany({
     where: {
       published: true,
       ...or,
+      ...difficultyFilter,
+      ...mealTypeFilter,
+      ...cuisineFilter,
+      ...sourcesFilter,
     },
     include: { author: true },
     take: Number(take) || undefined,
