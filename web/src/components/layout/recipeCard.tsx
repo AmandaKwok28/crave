ecbee994bef5c0ec3587662d36b09666db66a6a3
alatsina <103145967+alatsina@users.noreply.github.com>
2025-03-12 18:59:31 -0400

    updated recipe card ui to better display tags

--- web/src/components/layout/recipeCard.tsx
+++ web/src/components/layout/recipeCard.tsx
@@ -2,6 +2,7 @@ import { RecipeType } from "@/data/types";
 import { $router } from "@/lib/router";
 import { Card, Button, Image, Text, Tag, Box } from "@chakra-ui/react";
 import { redirectPage } from "@nanostores/router";
+import { Avatar, AvatarGroup } from "@chakra-ui/react"
 
 
 const RecipeCard = ({ recipe }: { recipe: RecipeType }) => {
@@ -14,6 +15,16 @@ const RecipeCard = ({ recipe }: { recipe: RecipeType }) => {
       redirectPage($router, 'editDraft', { draft_id: id });
     }
   }
+
+  const capitalizeFirstLetter = (str: string): string => 
+    str[0].toUpperCase() + str.slice(1).toLowerCase();
+
+  const priceMap: Record<string, string> = {
+    "CHEAP": "$",
+    "MODERATE": "$$",
+    "PRICEY": "$$$",
+    "EXPENSIVE": "$$$$"
+  };
 
   return (
     <Card.Root width="320px" maxH="500px" overflow="hidden">
@@ -32,7 +43,13 @@ const RecipeCard = ({ recipe }: { recipe: RecipeType }) => {
             </Text>
         </Card.Title>
         <Card.Description>
-            {recipe.author ? recipe.author.name : "hi"}
+          <AvatarGroup>
+            <Avatar.Root size="xs" variant="subtle">
+              <Avatar.Fallback name={`${recipe.author.name}`} />
+              <Avatar.Image />
+            </Avatar.Root>
+          </AvatarGroup>
+            {recipe.author ? `${recipe.author.name}` : "Guest"}
         </Card.Description>
         <Card.Description>
             <Text lineClamp="3">
@@ -41,26 +58,38 @@ const RecipeCard = ({ recipe }: { recipe: RecipeType }) => {
 
             {/* For the recipe card: let's only display popular tags like price, allergens, and preptime */}
             <Box flexWrap="true" spaceX="2" mt="4">
-            {recipe.allergens.map((tag, index) => (
+            {/* {recipe.allergens.map((tag, index) => (
                   <Tag.Root key={index} variant="outline" size="lg" w="fit-content" borderRadius="10px">
                       <Tag.Label>{tag}</Tag.Label>
                   </Tag.Root>
-            ))}
-             <Tag.Root variant="outline" size="lg" w="fit-content" borderRadius="10px">
-                <Tag.Label>{recipe.price}</Tag.Label>
-            </Tag.Root>
-            <Tag.Root variant="outline" size="lg" w="fit-content" borderRadius="10px">
-                <Tag.Label>{recipe.prepTime}</Tag.Label>
-            </Tag.Root>
+            ))} */}
+                {recipe.cuisine && (
+                  <Tag.Root size="lg" w="fit-content" borderRadius="10px" colorPalette={"blue"}>
+                    <Tag.Label>{capitalizeFirstLetter(recipe.cuisine)}</Tag.Label>
+                  </Tag.Root>
+                )}
+
+                {recipe.price && (
+                  <Tag.Root size="lg" w="fit-content" borderRadius="10px" colorPalette={"green"}>
+                    <Tag.Label>{priceMap[recipe.price]}</Tag.Label>
+                  </Tag.Root>
+                )}
+
+                {recipe.prepTime && (
+                  <Tag.Root size="lg" w="fit-content" borderRadius="10px" colorPalette={"purple"}>
+                    <Tag.Label>{`${recipe.prepTime} min`}</Tag.Label>
+                  </Tag.Root>
+                )}
             </Box>
         
         </Card.Description>
       </Card.Body>
-      <Card.Footer gap="2">
-        <Button variant="ghost" onClick={handleSeeMore}>
+      <Card.Footer>
+        <Button 
+          variant="ghost" 
+          onClick={handleSeeMore}>
           See More
         </Button>
-
       </Card.Footer>
     </Card.Root>
   );
