8a11c5d68a1f8f0e48a28d2db3980d3024a03f53
LiamH <nhousen1@jhu.edu>
2025-04-06 21:58:12 -0400

    updated recipe card margin

--- web/src/components/layout/recipeCard.tsx
+++ web/src/components/layout/recipeCard.tsx
@@ -94,7 +94,7 @@ const RecipeCard = ({ recipe }: { recipe: RecipeType }) => {
             </Text>
 
             {/* For the recipe card: let's only display popular tags like price, allergens, and preptime */}
-            <Box flexWrap="true" spaceX="2" mt="4">
+            <Box flexWrap="true" spaceX="2" mt="3">
             {/* {recipe.allergens.map((tag, index) => (
                   <Tag.Root key={index} variant="outline" size="lg" w="fit-content" borderRadius="10px">
                       <Tag.Label>{tag}</Tag.Label>
