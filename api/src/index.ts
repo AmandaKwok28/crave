0dbc2f80dfb4367057b6c70a9713456b89b664cc
Shawn <sshhaawwnn111@gmail.com>
2025-04-20 15:41:20 -0400

    remove unecessary code

--- api/src/index.ts
+++ api/src/index.ts
@@ -96,10 +96,6 @@ app.use('/like', likeRoutes);
 app.use('/bookmark', bookmarkRoutes);
 app.use('/gpt', gptRoutes);
 
-// Define __dirname for ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 startBackgroundJobs();
 
 app.listen(port, '0.0.0.0', () => {
